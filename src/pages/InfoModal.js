5850f4a634d2302dab7581bf7560e1111f515372
Quang Phu Ly <quangphuly150402@gmail.com>
2024-03-26 01:40:38 -0400

    Modify InfoModal

--- src/pages/InfoModal.js
+++ src/pages/InfoModal.js
@@ -15,14 +15,22 @@ const InfoModal = ({ isOpen, closeModal, title, content }) => {
             alignItems: 'center',
             justifyContent: 'center',
             textAlign: 'center',
-            width: '300px', // Adjust the width as needed
+            width: '400px', // Adjust the width as needed
             height: '500px', // Adjust the height as needed
             margin: 'auto', // Center the modal in the viewport
+            border: '2px solid black'
           },
         }}
       >
         <h2>{title}</h2>
-        <p>{content}</p>
+        <ul>
+          {content && Array.isArray(content) && content.map((data, index) => (
+            <React.Fragment key={index}>
+              <li style={{listStyleType: 'circle', marginTop: '15px'}}>{data}</li>
+              <hr style={{ width: '70%', margin: '5px auto', fontWeight: 'bold' }} />
+            </React.Fragment>
+          ))}
+        </ul>
         <button onClick={closeModal}>Close</button>
       </Modal>
     );
